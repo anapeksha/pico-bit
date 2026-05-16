33fd7b0a6972fc469bbbde124c40a48efb72d1ae
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2026-05-17 04:44:41 +0530

    fix: compile to output directly

--- c_modules/micropython.cmake
+++ c_modules/micropython.cmake
@@ -1,1 +1,25 @@
-include(${CMAKE_CURRENT_LIST_DIR}/usb_ncm/micropython.cmake)
+if(NOT TARGET usermod_usb_ncm)
+    # OBJECT library: sources compile to direct .o linker inputs, NOT into an
+    # archive. This ensures tud_network_recv_cb and the other TinyUSB NCM
+    # callbacks are unconditionally visible at link time, regardless of the
+    # order in which TinyUSB's ncm_device.c archive is scanned.
+    add_library(usermod_usb_ncm OBJECT
+        ${CMAKE_CURRENT_LIST_DIR}/usb_ncm/usb_ncm.c
+        ${CMAKE_CURRENT_LIST_DIR}/usb_ncm/usb_ncm_descriptors.c
+    )
+
+    # Inherit MicroPython/TinyUSB/lwIP include paths from usermod at build
+    # time via generator expression (evaluated during make, not cmake).
+    target_include_directories(usermod_usb_ncm PRIVATE
+        $<TARGET_PROPERTY:usermod,INTERFACE_INCLUDE_DIRECTORIES>
+        ${CMAKE_CURRENT_LIST_DIR}/usb_ncm
+    )
+
+    # Add the compiled object files directly to any target that links usermod.
+    target_link_libraries(usermod INTERFACE
+        $<TARGET_OBJECTS:usermod_usb_ncm>
+    )
+    target_include_directories(usermod INTERFACE
+        ${CMAKE_CURRENT_LIST_DIR}/usb_ncm
+    )
+endif()
--- c_modules/usb_ncm/micropython.cmake
+++ c_modules/usb_ncm/micropython.cmake
@@ -1,14 +1,1 @@
-if(NOT TARGET usermod_usb_ncm)
-    add_library(usermod_usb_ncm INTERFACE)
-
-    target_sources(usermod_usb_ncm INTERFACE
-        ${CMAKE_CURRENT_LIST_DIR}/usb_ncm.c
-        ${CMAKE_CURRENT_LIST_DIR}/usb_ncm_descriptors.c
-    )
-
-    target_include_directories(usermod_usb_ncm INTERFACE
-        ${CMAKE_CURRENT_LIST_DIR}
-    )
-
-    target_link_libraries(usermod INTERFACE usermod_usb_ncm)
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/../micropython.cmake)
